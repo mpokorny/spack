1f610cf87155a3e104a6acecc474822f9613e9f0
Martin Pokorny <martin@truffulatree.org>
2020-09-25 18:25:23 -0600

    Set BUILD_MARCH based on Spack architecture
    This supports consistency in the -march flag set by Legion CMake and the
    Spack compiler wrapper.

--- var/spack/repos/builtin/packages/legion/package.py
+++ var/spack/repos/builtin/packages/legion/package.py
@@ -256,6 +256,8 @@ class Legion(CMakePackage):
             maxfields = self.spec.variants['max-fields'].value
             option.append('-DLegion_MAX_FIELDS=%d' % maxfields)
 
+        options.append('-DBUILD_MARCH:STRING=%s' % self.spec.architecture.target)
+
         options.append('-DCMAKE_CXX_FLAGS=%s' % (" ".join(cmake_cxx_flags)))
 
         return options
